7429fe8481a6cde8945c57a3e3c63e22d95abfec
sakib <uschas75@gmail.com>
2025-08-07 16:56:14 +0200

    task 3

--- 8kyu/basicControlLogic.js
+++ 8kyu/basicControlLogic.js
@@ -19,5 +19,31 @@ function typeValidation(variable, type) {
   return typeof variable === type;
 }
 
-
 console.log("typeValidation:" + typeValidation(5, "number"));
+
+/**
+ * Task:3
+The function pickIt accepts 1 parameter, arr, which is an array of numbers.
+ We need to traverse arr by using a for loop. If an element is an odd number, 
+ push it to the odd array, and if it's an even number, push it to the evenarray.
+
+I've defined two arrays odd and even in the function, and also wrote the return 
+statement. Your work is to write a for loop.
+ * 
+ */
+function pickIt(arr) {
+  let odd = [],
+    even = [];
+  //coding here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+
+  console.log(odd, even);
+  return [odd, even];
+}
+console.log("pickIt:" + pickIt([1, 2, 3, 4, 5, 6, 7, 8, 9]));
