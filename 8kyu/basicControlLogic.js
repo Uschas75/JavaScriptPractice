0d62c0de41f4971a35a263c53c6eb77acd6e63f3
sakib <uschas75@gmail.com>
2025-08-10 17:18:42 +0200

    task 13

--- 8kyu/basicControlLogic.js
+++ 8kyu/basicControlLogic.js
@@ -230,7 +230,7 @@ function padIt(str, n) {
 
   return str;
 }
-console.log("padIt:" + padIt("a", 5)); // Expected output: "*a**"
+// console.log("padIt:" + padIt("a", 5)); // Expected output: "*a**"
 
 /**
  * 
@@ -342,7 +342,7 @@ let sum = 0;
   console.log("result" + sum);
   sum += i + 1; // adds 1..5 to sum
 });
-console.log(sum);
+// console.log(sum);
 
 /**
  *
@@ -360,3 +360,29 @@ function findStrangeValue() {
   console.log(x === x); // false
   return x;
 }
+
+/**
+ * Task:13
+ * You will be given an array (a) and a limit value (limit)..
+ *  You must check that all values in the array are below or 
+ * equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+Do not use loops. Do not modify input array.
+ */
+function smallEnough(a, limit, index = 0) {
+  // Base case: if we've checked all elements, return true
+  if (index === a.length) {
+    return true;
+  }
+
+  // If current element exceeds limit, return false immediately
+  if (a[index] > limit) {
+    return false;
+  }
+
+  // Recursively check next element
+  return smallEnough(a, limit, index + 1);
+}
+console.log("result: " + smallEnough([1, 2, 3, 4, 4], 5)); // Expected output: true
