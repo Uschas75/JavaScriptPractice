0e07fa3d7e9371825fb92589d60064518828b280
sakib <uschas75@gmail.com>
2025-08-11 23:58:20 +0200

    task 1-7kyu-BCL

--- 8kyu/basicControlLogic8kyu.js
+++ 8kyu/basicControlLogic8kyu.js
@@ -361,28 +361,5 @@ function findStrangeValue() {
   return x;
 }
 
-/**
- * Task:13
- * You will be given an array (a) and a limit value (limit)..
- *  You must check that all values in the array are below or 
- * equal to the limit value. If they are, return true. Else, return false.
 
-You can assume all values in the array are numbers.
-
-Do not use loops. Do not modify input array.
- */
-function smallEnough(a, limit, index = 0) {
-  // Base case: if we've checked all elements, return true
-  if (index === a.length) {
-    return true;
-  }
-
-  // If current element exceeds limit, return false immediately
-  if (a[index] > limit) {
-    return false;
-  }
-
-  // Recursively check next element
-  return smallEnough(a, limit, index + 1);
-}
 console.log("result: " + smallEnough([1, 2, 3, 4, 4], 5)); // Expected output: true
