e5ac7fd1d7205ec3310e82e787c36c9688868d06
sakib <uschas75@gmail.com>
2025-08-16 23:57:58 +0200

    task 5-7kyu-BCL

--- 7kyu/basicControlLogic7kyu.js
+++ 7kyu/basicControlLogic7kyu.js
@@ -138,3 +138,60 @@ solution(5); // should return "Value is 00005"
 
 console.log(solution("8")); // solution("12345"); // Expected output: "Value is 12345"
 
+
+
+/**
+ * task:5
+ * 
+Task
+
+Given an array of length N, where N denotes the number 
+of iterative statements. Each item-pair in the array 
+represents two elements, with the 1st value (V) indicating
+ the upper boundary for the iteration to take place 
+ (can be inclusive or exclusive depending on the 2nd value) 
+ and the 2nd value (Boolean data type -> true / false depending 
+ on your chosen language) indicating whether the upper boundary (V) is inclusive or not.
+
+You must write a function that outputs an array in which each 
+element represents the number of times each for-loop condition is evaluated. 
+Below is an example for better understanding:
+
+Example
+
+arr = [[7, true], [5, false]]
+
+for(int i = 0; i <= 7; i++){   // This statement is executed 9 times 
+// before termination -> 0, 1, 2, 3, 4, 5, 6, 7, 8 (since 8 > 7 is the breaking condition)
+  for(int j = 0; j < 5; j++){  // In one cycle of outermost loop, this statement 
+  // is executed 6 times before termination -> 0, 1, 2, 3, 4, 5 (since 5 >= 5 is the breaking condition)
+    // some statements
+  }
+}
+Note
+
+The array can be empty, with a range of 0 <= N <= 20
+The starting counter of the C-style for-loop is always 0
+The iteration expression or operation to be performed is always incremental
+The range of upper boundary is as follows: 1 <= V <= 20
+}
+ */
+
+function countLoopIterations(arr){
+  let result = [];
+  let outerProduct = 1n;
+
+  for (const [Vraw, inc] of arr) {
+    const V = BigInt(Vraw);
+    const iterations = inc ? V + 1n : V;          // 0..V (inc) or 0..V-1 (exc)
+    const perRunChecks = iterations + 1n;         // extra failing check
+    const totalChecks = perRunChecks * outerProduct;
+
+    result.push(totalChecks);
+    outerProduct *= iterations;
+  }
+  return result;
+}
+
+// Example usage
+console.log(countLoopIterations([[7, true], [5, false]])); // Expected output: [9, 6]
