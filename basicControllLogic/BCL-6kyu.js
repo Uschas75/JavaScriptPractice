38dc53492d946c139f9316bd23c336bfd7c988e3
sakib <uschas75@gmail.com>
2025-09-01 23:47:48 +0200

    Less work

--- basicControllLogic/BCL-6kyu.js
+++ basicControllLogic/BCL-6kyu.js
@@ -507,3 +507,19 @@ function differentiate(f) {
   return `${n * a}x^${n -1}`;
 }
 console.log(differentiate("x^-2")); // '20x^3'
+/**
+ * task: 33
+ * In this kata you're given a string which can include English
+ *  lowercase letters, digits, and spaces. Your task is to write a function 
+ * which will return a string of 'L' and 'R' chars which displays in which order should the hands be used to type it.
+
+Rules of touch typing
+The digits before (including) 5, letters of the first row before (including) t, 
+letters of the second row before (including) g and letters of the third row before (including) b are typed with left hand, the other digits and numbers are typed with right hand.
+Space is typed with left hand if the non-space char you typed before was typed with right one, and vice versa. If it is the first char, it's typed with left hand.
+Every string you're given is a correct string which includes only digits, lowercase letters and spaces. Strings need not be validated.
+
+Examples
+"i love programming" -> "RLRRLLRRLRLLLRRRRL"
+"  two spaces" -> "LLLLRLLRLLLL"
+ */
